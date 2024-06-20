71890392808bda0cd51e43fefe953f573fe748db
Mbengi <mbengiivy@gmail.com>
2024-06-20 09:24:19 +0300

    Updated CRUD functionalities

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Product
+from .forms import ProductForm
+
 
 # Create your views here.
 
@@ -15,12 +17,45 @@ def staff(request):
 
 @login_required(login_url='user-login')
 def product(request):
-    items = Product.objects.all()
+    items = Product.objects.all()#using ORM
+    #items = Product.objects.raw('SELECT * FROM dashboard_product')#using SQL
+
+    if request.method=='POST':
+        form=ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard-product')
+    else:
+        form = ProductForm()
+
     context = {
         'items': items,
+        'form': form,
     }
-    return render(request,'dashboard/product.html')  
+    return render(request,'dashboard/product.html', context)  
 
 @login_required(login_url='user-login')
 def order(request):
     return render(request,'dashboard/order.html')
+
+
+def product_delete(request, pk):
+    item = Product.objects.get(id=pk) 
+    if request.method=='POST':
+        item.delete()
+        return redirect('dashboard-product')
+    return render(request,'dashboard/product_delete.html')
+
+def product_update(request, pk):
+    item = Product.objects.get(id=pk)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard-products')
+    else:
+        form = ProductForm(instance=item)
+    context = {
+        'form': form,
+    }
+    return render(request, 'dashboard/product _update.html', context) 
