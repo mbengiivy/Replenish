69b50c5d468f07d5c2b99a2afdfc16633fb99dfb
Mbengiivy <mbengiivy@gmail.com>
2025-11-19 09:30:18 +0300

    edited path url

--- backend/replenish/urls.py
+++ backend/replenish/urls.py
@@ -3,12 +3,12 @@ from django.urls import path, include
 from django.http import JsonResponse
 
 # Define a simple view function (can be placed in any app's views.py or here)
-def health_check(request):
-    return JsonResponse({"status": "ok", "message": "Backend is running"})
+# def health_check(request):
+#     return JsonResponse({"status": "ok", "message": "Backend is running"})
 
 urlpatterns = [
 path('admin/', admin.site.urls),
-#path('api/', include('api.urls')),
+path('api/', include('api.urls')),
 path("api/auth/", include("authentication.urls")),
-path("", health_check, name="root_status"), # This catches the root path '/'
+#path("", health_check, name="root_status"), # This catches the root path '/'
 ]
